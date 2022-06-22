7d0af1a1cb0ffd7238bd5c0e131be014adf07313
Prudentus <prudoctus01@gmail.com>
2022-06-22 22:45:03 +0100

    initial commit

---
+++ mentor.js
@@ -0,0 +1,11 @@
+let result;
+const pharm = prompt(`Please input your name`);
+// console.log();
+
+alert(
+  "Pharmacist " +
+    " " +
+    pharm +
+    " " +
+    "You have done alot for us ,thank you very much for the motivation to read.We love and appreciate you ma"
+);
